7e78ddfbdff3057aa780ab12c7cb169bc9a66783
maximandreev <maxands@yandex.ru>
2022-09-04 11:35:27 +0300

    Task 1 completed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,12 +1,25 @@
 ﻿void FillArray(int[,] array)
 {
-    for (int i=0; i<array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j=0; j<array.GetLength(1); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
             array[i, j] = new Random().Next(1, 10);
         }
     }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
 }
 
 
@@ -16,8 +29,22 @@ Console.WriteLine("Введите количество столбцов:");
 int columns = Convert.ToInt32(Console.ReadLine());
 int[,] array = new int[rows, columns];
 FillArray(array);
-for (int i=0; i<array.GetLength(0); i++)
+PrintArray(array);
+Console.WriteLine();
+int temporary;
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    array[i] = SortArray(array[i]);
-    
+    for (int l = 0; l < array.GetLength(1); l++)
+    {
+        for (int j = l + 1; j < array.GetLength(1); j++)
+        {
+            if (array[i, l] < array[i, j])
+            {
+                temporary = array[i, l];
+                array[i, l] = array[i, j];
+                array[i, j] = temporary;
+            }
+        }
+    }
 }
+PrintArray(array);
