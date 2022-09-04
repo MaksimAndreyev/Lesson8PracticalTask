57bcb6b5c32c74e0db7ee0da201d3e8357a7bc95
maximandreev <maxands@yandex.ru>
2022-09-04 11:58:06 +0300

    Task 2 completed

---
+++ Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+}
+
+
+Console.WriteLine("Введите количесвто строк:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int sum = 0;
+int minSum = 32000;
+int minRow = 1;
+for (int i=0; i<array.GetLength(0); i++)
+{
+    for (int j=0; j<array.GetLength(1); j++)
+    {
+        sum += array[i, j];
+    }
+    if (sum < minSum)
+    {
+        minSum = sum;
+        minRow = i+1;
+    }
+    sum = 0;
+}
+Console.WriteLine($"{minRow} строка имеет наименьшую сумму элементов.");
